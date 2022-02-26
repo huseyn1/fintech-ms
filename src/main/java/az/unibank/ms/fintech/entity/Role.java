34cd194a57b7710887f437408434daeac418e290
huseyn1 <huseynov.sys@gmail.com>
2022-02-26 13:06:21 +0400

    inital commit

--- src/main/java/az/unibank/ms/fintech/entity/Role.java
+++ src/main/java/az/unibank/ms/fintech/entity/Role.java
@@ -1,4 +1,9 @@
 package az.unibank.ms.fintech.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
 public class Role {
 }
--- src/main/java/az/unibank/ms/fintech/entity/User.java
+++ src/main/java/az/unibank/ms/fintech/entity/User.java
@@ -1,4 +1,33 @@
 package az.unibank.ms.fintech.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    private String id;
+
+    @Column(name = "pin")
+    private String pin;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+
+
 }
